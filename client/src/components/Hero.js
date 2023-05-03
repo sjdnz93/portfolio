0b98b6a83e3043c5ef7bcafa73bd132436f94125
Simon Davis <s.davis@hotmail.co.nz>
2023-05-03 16:40:42 +0100

    some basic styling hero

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,10 +1,13 @@
+import profile from '../images/profile.jpeg'
+
 
 const Hero = () => {
 
   return (
 
     <section id='hero'>
-      <h1>HERO</h1>
+      <img src={profile}></img>
+      <h1>Hi, my name is Simon. I&apos;m a full-stack developer</h1>
     </section>
   )
 }
