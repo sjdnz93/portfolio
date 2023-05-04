3a89218c521ccd6f3296d5c524a8a0d6742de9ea
Simon Davis <s.davis@hotmail.co.nz>
2023-05-04 15:36:10 +0100

    about section styled for mobile

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,18 @@ const About = () => {
 
   return (
     <section id='about'>
-      <h1>ABOUT</h1>
+
+      <div className='about-h2-container'>
+        <h2>.about</h2>
+      </div>
+
+      <div className='about-content-container'>
+        <p>As a former automotive journalist, making the career pivot into software engineering by joining General Assembly&apos;s three&#45;month intensive bootcamp felt like a pretty logical step. </p>
+        <p>Not only am I at home working to tight deadlines, I also bring with me a skill-set that prioritises research, collaboration, relationship building and critical thinking in both a logical and creative sense.</p>
+        <p>I have a keen appetite for acquiring knowledge and a desire to learn a new set of hard skills in a field I find extremely fascinating&#59; this is ultimately what spurred me on to make the jump. Now that I&apos;m here the almost unlimited scope for further learning and development really excites me. I want to build cool stuff with all of these newly&#45;acquired skills&#33;</p>
+        <p>I&apos;m looking for opportunities as a software engineer or full&#45;stack developer that will allow me to hone and deploy my skills, and to learn from new teammates who bring a wealth of experience from the field to the table.</p>
+      </div>
+
     </section>
   )
 }
