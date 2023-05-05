8c6fa885768c7a827ad99ab0027be7bbfe1ac14f
Simon Davis <s.davis@hotmail.co.nz>
2023-05-05 11:19:39 +0100

    skills section basic styling optimised for mobile and desktop

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,9 +1,18 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
 
-import { DiBootstrap, DiCss3, DiDjango, DiGithubBadge, DiHtml5, DiJsBadge, DiMongodb, DiNodejsSmall, DiNpm, DiPostgresql, DiPython, DiReact, DiSass } from 'react-icons/di'
+import { DiBootstrap, DiCss3, DiDjango, DiGithubBadge, DiHtml5, DiJsBadge, DiMongodb, DiNodejsSmall, DiNpm, DiPostgresql, DiPython, DiReact, DiSass, DiTrello } from 'react-icons/di'
 import { SiExpress, SiInsomnia, SiTypescript } from 'react-icons/si'
+import { AiOutlineApi, AiOutlineTeam, AiOutlineUnlock, AiOutlineComment } from 'react-icons/ai'
+import { VscVersions } from 'react-icons/vsc'
+import { GiWireframeGlobe, GiTeamIdea } from 'react-icons/gi'
+import { GrCircleAlert } from 'react-icons/gr'
+import { RiTeamLine } from 'react-icons/ri'
+import { TbWritingSign } from 'react-icons/tb'
+
+import Mongoose from '../images/Mongoose.js.svg'
 
 const Skills = () => {
 
@@ -17,33 +26,38 @@ const Skills = () => {
       <Container className='skills-content-container'>
         <Row className='skills-row'>
 
-          <Col>
+          <Col className='h3-col'>
             <h3>Front-end:</h3>
           </Col>
 
           <Row className='skills-row-wrapper'>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiReact size='100px'></DiReact>
+              <p>React</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiJsBadge size='100px'></DiJsBadge>
+              <p>JavaScript</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiHtml5 size='100px'></DiHtml5>
+              <p>HTML</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiCss3 size='100px'></DiCss3>
+              <p>CSS</p>
             </Col>
 
-            <Col>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiSass size='100px'></DiSass>
+              <p>Sass</p>
+            </Col>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
               <DiBootstrap size='100px'></DiBootstrap>
               <p>Bootstrap</p>
             </Col>
@@ -54,35 +68,45 @@ const Skills = () => {
 
         <Row className='skills-row'>
 
-          <Col>
+          <Col className='h3-col'>
             <h3>Back-end:</h3>
           </Col>
 
           <Row className='skills-row-wrapper'>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiPython size='100px'></DiPython>
+              <p>Python</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiDjango size='100px'></DiDjango>
+              <p>Django</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiPostgresql size='100px'></DiPostgresql>
+              <p>PostgreSQL</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiMongodb size='100px'></DiMongodb>
+              <p>MongoDB</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <Image src={Mongoose} size='100px' />
+              <p>Mongoose.js</p>
+            </Col>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <SiExpress size='100px'></SiExpress>
+              <p>Express.js</p>
+            </Col>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiNodejsSmall size='100px'></DiNodejsSmall>
+              <p>Node.js</p>
             </Col>
 
           </Row>
@@ -91,35 +115,87 @@ const Skills = () => {
 
         <Row className='skills-row'>
 
-          <Col>
+          <Col className='h3-col'>
+            <h3>Other:</h3>
+          </Col>
+
+          <Row className='skills-row-wrapper'>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <SiInsomnia size='100px'></SiInsomnia>
+              <p>Insomnia</p>
+            </Col>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <AiOutlineApi size='100px'></AiOutlineApi>
+              <p>RESTful APIs</p>
+            </Col>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiGithubBadge size='100px'></DiGithubBadge>
+              <p>GitHub</p>
+            </Col>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <VscVersions size='100px'></VscVersions>
+              <p>Version control</p>
+            </Col>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <AiOutlineTeam size='100px'></AiOutlineTeam>
+              <p>Pair-programming</p>
+            </Col>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <AiOutlineUnlock size='100px'></AiOutlineUnlock>
+              <p>Authentications & permissions</p>
+            </Col>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <GiWireframeGlobe size='100px'></GiWireframeGlobe>
+              <p>Wireframing</p>
+            </Col>
+
+          </Row>
+
+        </Row>
+
+        <Row className='skills-row'>
+
+          <Col className='h3-col'>
             <h3>Soft skills:</h3>
           </Col>
 
           <Row className='skills-row-wrapper'>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <DiTrello size='100px'></DiTrello>
+              <p>Project management</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <GrCircleAlert size='100px'></GrCircleAlert>
+              <p>Problem solving</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <RiTeamLine size='100px'></RiTeamLine>
+              <p>Leadership</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <AiOutlineComment size='100px'></AiOutlineComment>
+              <p>Communication</p>
             </Col>
 
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <GiTeamIdea size='100px'></GiTeamIdea>
+              <p>Collaboration</p>
+            </Col>
+
+            <Col xs={4} sm={4} md={2} lg={2} xl={2}>
+              <TbWritingSign size='100px'></TbWritingSign>
+              <p>Creative writing</p>
             </Col>
 
           </Row>
@@ -128,34 +204,14 @@ const Skills = () => {
 
         <Row className='skills-row'>
 
-          <Col>
+          <Col className='h3-col'>
             <h3>Currently learning:</h3>
           </Col>
 
           <Row className='skills-row-wrapper'>
 
             <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
-            </Col>
-
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
-            </Col>
-
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
-            </Col>
-
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
-              <p>Bootstrap</p>
-            </Col>
-
-            <Col>
-              <DiBootstrap size='100px'></DiBootstrap>
+              <SiTypescript size='100px'></SiTypescript>
               <p>Bootstrap</p>
             </Col>
 
