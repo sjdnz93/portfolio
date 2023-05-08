b8ebb58b598e35d7d6c630a05789f64b8265b513
Simon Davis <s.davis@hotmail.co.nz>
2023-05-08 13:48:08 +0100

    waves fixed for mobile

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,9 @@ import svg2 from './images/SVG/2-svg.svg'
 import svg6 from './images/SVG/6-svg.svg'
 import svg8 from './images/SVG/8-svg.svg'
 import wave from './images/SVG/wave-svg.svg'
-import wave1 from './images/SVG/wave-1-svg.svg'
+import wave4 from './images/SVG/wave-4-svg-svg.svg'
 import wave2 from './images/SVG/wave-2-svg.svg'
-
+import wave3 from './images/SVG/wave-3-svg.svg'
 
 const App = () => {
 
@@ -27,7 +27,7 @@ const App = () => {
       <main>
         <Hero />
 
-        <img src={wave} alt='wave-effect' className='wave'></img>
+        <img src={wave3} alt='wave-effect' className='wave'></img>
         <About />
 
         <img src={svg2} alt='drip-effect' className='drip'></img>
@@ -35,7 +35,7 @@ const App = () => {
         <Skills />
 
         <img src={svg6} alt='drip-effect' className='drip'></img>
-        <img src={wave1} alt='wave-effect' className='wave'></img>
+        <img src={wave4} alt='wave-effect' className='wave'></img>
         <Projects />
 
         <img src={svg8} alt='drip-effect' className='drip'></img>
